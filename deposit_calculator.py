6dffe348125ee36e71e2cbff635a3811bc591c9d
Ivan PETKOV <ivn5e7k0v@gmail.com>
2023-01-05 01:21:05 +0200

    tasks

--- deposit_calculator.py
+++
@@ -1,7 +0,0 @@
-deposit_sum = float(input())
-period_month = int(input())
-year_devident = float(input())
-mont_devident = deposit_sum * year_devident / 100 / 12
-
-amount = deposit_sum + (period_month * mont_devident)
-print(amount)
