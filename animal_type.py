101650adf846ba7ba2fdcce24489ae6faeeaad44
Ivan PETKOV <ivn5e7k0v@gmail.com>
2023-01-16 19:30:08 +0200

    LAB

---
+++ animal_type.py
@@ -0,0 +1,8 @@
+name_animmal = input()
+
+if name_animmal == "dog":
+    print("mammal")
+elif name_animmal == "crocodile" or name_animmal == "tortoise" or name_animmal == "snake":
+    print("reptile")
+else:
+    print("unknown")
