d9057c1de15a61184bbe52c1d382d523d3740b26
Ivan PETKOV <ivn5e7k0v@gmail.com>
2023-01-16 19:30:39 +0200

    LAB

---
+++ cinema_ticket.py
@@ -0,0 +1,23 @@
+day_of_week = input()
+if day_of_week in ["Monday", "Tuesday", "Friday"]:
+    print(12)
+elif day_of_week in ["Wednesday", "Thursday"]:
+    print(14)
+elif day_of_week in ["Saturday", "Sunday"]:
+    print(16)
+# if day_of_week == "Monday":
+#     print(12)
+# elif day_of_week == "Tuesday":
+#     print(12)
+# elif day_of_week == "Wednesday":
+#     print(14)
+# elif day_of_week == "Thursday":
+#     print(14)
+# elif day_of_week == "Friday":
+#     print(12)
+# elif day_of_week == "Saturday":
+#     print(16)
+# elif day_of_week == "Sunday":
+#     print(16)
+
+
