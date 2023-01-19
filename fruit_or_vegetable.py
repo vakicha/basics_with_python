9e435949006b9c09afd0dc610f287613092b04fb
Ivan PETKOV <ivn5e7k0v@gmail.com>
2023-01-19 21:09:48 +0200

    lab

---
+++ fruit_or_vegetable.py
@@ -0,0 +1,11 @@
+product_name = input()
+if product_name in ["banana", "apple", "kiwi", "cherry", "lemon", "grapes"]:
+    print("fruit")
+elif product_name in ["tomato", "cucumber", "pepper", "carrot"]:
+    print("vegetable")
+else:
+    print("unknown")
+
+
+
+
