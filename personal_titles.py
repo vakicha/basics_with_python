dee9c39c68ebdaea2b91109a1220e83baebb88e9
Ivan PETKOV <ivn5e7k0v@gmail.com>
2023-01-20 19:16:49 +0200

    lab

---
+++ personal_titles.py
@@ -0,0 +1,17 @@
+age = float(input())
+gender = input()
+if gender == "f":
+    if age >= 16:
+        print("Ms.")
+    else:
+        print("Miss")
+if gender == "m":
+    if age >= 16:
+        print("Mr.")
+    else:
+        print("Master")
+
+
+
+
+
