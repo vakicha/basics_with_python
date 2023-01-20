a0101ebee3c4b1589a41457cb3fa84ba08e32c55
Ivan PETKOV <ivn5e7k0v@gmail.com>
2023-01-20 19:17:20 +0200

    lab

---
+++ projects_creation.py
@@ -0,0 +1,4 @@
+arch_name = input()
+number_of_projects = int(input())
+needed_hours = number_of_projects * 3
+print(f"The architect {arch_name} will need {needed_hours} hours to complete {number_of_projects} project/s.")
